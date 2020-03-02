2fb85444d578b8469f80c076a894823bc16a37ab
ymdx <y_m_d_x@163.com>
2020-03-02 08:05:59 +0800

    update springboot-mongodb

--- springboot-mongodb/src/main/java/com/ymdx/App.java
+++ springboot-mongodb/src/main/java/com/ymdx/App.java
@@ -1,7 +1,15 @@
 package com.ymdx;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * @ClassName: App
@@ -12,8 +20,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version: 1.0
  **/
 @SpringBootApplication
-public class App {
+@MapperScan("com.ymdx.dao")
+public class App extends WebMvcConfigurationSupport {
 
+    @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 创建FastJson的消息转换器
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        // 创建FastJson的配置对象
+        FastJsonConfig config = new FastJsonConfig();
+        // 对Json数据进行格式化
+        config.setSerializerFeatures(SerializerFeature.PrettyFormat);
+
+        converter.setFastJsonConfig(config);
+        converters.add(converter);
+    }
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
